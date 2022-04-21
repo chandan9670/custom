51eebdcfb8576e156354661b024a9a351d9841c3
chandan9670 <91278927+chandan9670@users.noreply.github.com>
2022-04-21 16:05:56 +0530

    Add files via upload

---
+++ src/main/java/com/tech/custom/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.tech.custom.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http
+			.authorizeRequests()
+			.antMatchers("/public/**").hasRole("admin")
+			.anyRequest()
+			.authenticated()
+			.and()
+			.httpBasic();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("abc").password("abc123").roles("admin");
+		auth.inMemoryAuthentication().withUser("xyz").password("xyz123").roles("admin");
+
+	}
+	
+	@Bean
+	public PasswordEncoder encoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+}
